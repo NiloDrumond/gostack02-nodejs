15efc66d76c487e9312f933e87eabe0f97231423
Nilo <nilobdrumond@gmail.com>
2020-04-14 12:02:41 -0300

    Desafio concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, { value, type }) => {
+      if (type === 'income') {
+        return acc + value;
+      }
+
+      return acc;
+    }, 0);
+    const outcome = this.transactions.reduce((acc, { value, type }) => {
+      if (type === 'outcome') {
+        return acc + value;
+      }
+
+      return acc;
+    }, 0);
+    const total = income - outcome;
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    if (
+      transaction.type === 'outcome' &&
+      transaction.value > this.getBalance().total
+    ) {
+      throw Error('Not enough cash in your balance');
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
